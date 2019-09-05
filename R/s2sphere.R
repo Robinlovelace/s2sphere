360be7c3fbdc519589129db1e8e6e057cb21ab63
Robin Lovelace <rob00x@gmail.com>
2019-09-05 11:16:54 +0100

    add load code

--- R/s2sphere.R
+++ R/s2sphere.R
@@ -1,1 +1,11 @@
 # code here
+
+# global reference to scipy (will be initialized in .onLoad)
+scipy <- NULL
+
+.onLoad <- function(libname, pkgname) {
+  # use superassignment to update global reference to s2s
+  s2s <<- reticulate::import("s2sphere"
+                             # , delay_load = TRUE
+                             )
+}
